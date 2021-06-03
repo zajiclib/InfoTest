4891f84418ed3d7e603e7df3a5a5aed12240d300
lzajicek <libor.zajicek@kaprion.de>
2021-06-03 10:11:20 +0200

    arrow image changed only to down arrow (easier computation)
    Changes to code

--- app/src/main/java/com/zajiclib/infotest/HelpScreen.java
+++ app/src/main/java/com/zajiclib/infotest/HelpScreen.java
@@ -54,142 +54,50 @@ public class HelpScreen implements View.OnClickListener {
     }
 
 
-    public void setDisplayAllTogether(boolean displayAllTogether) {
-        this.displayAllTogether = displayAllTogether;
-    }
-
-
-    /**
-     * Display all views together on one screen
-     */
-    public void displayAllHelp() {
-
-        for (int i = 0; i < viewsToShowHelp.size(); i++) {
-
-            View view = viewsToShowHelp.get(i);
-            ImageView ivCircle = new ImageView(context);
-            ImageView ivArrow = new ImageView(context);
-            TextView tvHelp = new TextView(context);
-
-            int viewWidth = view.getWidth();
-            int viewHeight = view.getHeight();
-
-            Drawable circle = ContextCompat.getDrawable(context, R.drawable.circle_custom);
-            Drawable arrow = ContextCompat.getDrawable(context, R.drawable.arrow_custom);
-            ivArrow.setImageDrawable(arrow);
-            ivArrow.requestLayout();
-            ivArrow.setId(View.generateViewId());
-
-            ivCircle.setImageDrawable(circle);
-            ivCircle.requestLayout();
-            ivCircle.setId(View.generateViewId());
-
-            tvHelp.setId(View.generateViewId());
-            tvHelp.setText(helpMessages.get(i));
-            tvHelp.setTextColor(Color.WHITE);
-
-            // image for circle
-            RelativeLayout.LayoutParams paramsIv = new RelativeLayout.LayoutParams((int) (viewWidth * ADJUST_RATIO), (int) (viewHeight * ADJUST_RATIO));
-
-            // calculating center coords for the view
-            int centerX, centerY;
-            centerX = (int) (view.getX() + (viewWidth / 2));
-            centerY = (int) (view.getY() + (viewHeight / 2));
-
-            int absolutePositionX = centerX - (int) ((viewWidth * ADJUST_RATIO) / 2 );
-            int absolutePositionY = centerY - (int) ((viewHeight * ADJUST_RATIO) / 2);
-
-            paramsIv.leftMargin = absolutePositionX;
-            paramsIv.topMargin = absolutePositionY;
-
-            helpLayout.addView(ivCircle, paramsIv);
-
-            // arrow params
-            RelativeLayout.LayoutParams paramsIvArrow = new RelativeLayout.LayoutParams((int) (viewWidth), (int) (viewHeight));
-
-            paramsIvArrow.leftMargin = (int) (absolutePositionX - viewWidth);
-            paramsIvArrow.topMargin = (int) (absolutePositionY + (viewHeight / 2));
-
-            helpLayout.addView(ivArrow, paramsIvArrow);
-
-            // textview params
-            RelativeLayout.LayoutParams paramsForTextV = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            int height, width;
-            tvHelp.measure(0,0);
-            height = tvHelp.getMeasuredHeight();
-            width = tvHelp.getMeasuredWidth();
-            paramsForTextV.leftMargin = (int) (ivArrow.getX() - width);
-            paramsForTextV.topMargin = (int) ivArrow.getY() - 20;
-
-            helpLayout.addView(tvHelp, paramsForTextV);
-
-        }
-
-        helpLayout.setVisibility(View.VISIBLE);
-    }
-
     /**
      * Method for displaying help in appropriate form
      */
     public void displayHelp() {
         for (int i = 0; i < viewsToShowHelp.size(); i++) {
 
+            // retrieving view to annotate
             View currentViewToAnnotate = viewsToShowHelp.get(i);
-            boolean isRight = isInRightHalf(currentViewToAnnotate);
             boolean isBottom = isOnBottomSide(currentViewToAnnotate);
-            boolean isMiddle = isInMiddle(currentViewToAnnotate);
 
             ImageView iv = new ImageView(context);
             iv.setAdjustViewBounds(true);
+//            iv.setBackgroundColor(Color.RED);
             iv.requestLayout();
-            TextView tv = new TextView(context);
-            tv.setTextColor(Color.WHITE);
-            tv.setText(helpMessages.get(i));
-            tv.requestLayout();
-            Drawable drawable;
-            if (isMiddle) {
-                drawable = ContextCompat.getDrawable(context, R.drawable.arrow_down);
-            } else {
-                drawable = ContextCompat.getDrawable(context, R.drawable.arrow_custom);
-            }
-
+            Drawable drawable = ContextCompat.getDrawable(context, R.drawable.arrow_down);
             iv.setImageDrawable(drawable);
-
-            // flip horizontally
-            if (!isRight) iv.setScaleX(-1.0f);
 
             // flip vertically
             if (!isBottom) iv.setScaleY(-1.0f);
 
-
             // arrow params
-            RelativeLayout.LayoutParams paramsArrow = new RelativeLayout.LayoutParams((int) (currentViewToAnnotate.getWidth() * ADJUST_RATIO), (int) (currentViewToAnnotate.getHeight() * ADJUST_RATIO));
+            RelativeLayout.LayoutParams paramsArrow = new RelativeLayout.LayoutParams((int) (currentViewToAnnotate.getWidth()), (int) (currentViewToAnnotate.getHeight()));
 
-            paramsArrow.leftMargin = computeLeftMargin(currentViewToAnnotate, iv);
-            paramsArrow.topMargin = computeTopMargin(currentViewToAnnotate, iv);
+            paramsArrow.leftMargin = computeLeftMargin(currentViewToAnnotate);
+            paramsArrow.topMargin = computeTopMargin(currentViewToAnnotate);
 
             helpLayout.addView(iv, paramsArrow);
 
+
+            // setting up the annotation text view
+            TextView tv = new TextView(context);
+            tv.setTextColor(Color.WHITE);
+            tv.setText(helpMessages.get(i));
+            tv.requestLayout();
+
+            RelativeLayout.LayoutParams paramsTv = new RelativeLayout.LayoutParams((int) currentViewToAnnotate.getWidth(), ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            paramsTv.leftMargin = computeLeftMargin(currentViewToAnnotate);
+            paramsTv.topMargin = computeTopMargin(currentViewToAnnotate, tv,  true);
+
+            helpLayout.addView(tv, paramsTv);
+
             helpLayout.setVisibility(View.VISIBLE);
         }
-    }
-
-    /**
-     * Decides if the view is located on the right half or not
-     */
-    public boolean isInRightHalf(@NonNull View view) {
-        int centerX = (int) view.getX() + view.getWidth() / 2;
-
-        return centerX >= displayWidth;
-    }
-
-    /**
-     * Checks whether is the view right in the middle or not (because of the different drawable used)
-     */
-    public boolean isInMiddle(@NonNull View view) {
-        int centerX = (int) view.getX() + view.getWidth() / 2;
-
-        return centerX == displayWidth / 2;
     }
 
     /**
@@ -204,32 +112,36 @@ public class HelpScreen implements View.OnClickListener {
     /**
      * Computes left margin for the arrow
      */
-    public int computeLeftMargin(View view, ImageView iv) {
-        boolean isRight = isInRightHalf(view);
-        boolean isMiddle = isInMiddle(view);
-
-        if (isMiddle) {
-            return (int) view.getX() + view.getWidth() / 2;
-        }
-
-        if (isRight) {
-            return (int) view.getX();
-        }
-
-        return (int) view.getX() + view.getWidth();
+    public int computeLeftMargin(View view) {
+        return (int) view.getX();
     }
 
     /**
      * Computes top margin for the arrow
      */
-    public int computeTopMargin(View view, ImageView imageView) {
+    public int computeTopMargin(View view) {
         boolean isBottom = isOnBottomSide(view);
 
         if (isBottom) {
-            return (int) view.getX() - imageView.getHeight();
+            return (int) view.getY() - view.getHeight();
         }
 
-        return (int) view.getX() + view.getHeight();
+        return (int) view.getY() + view.getHeight();
+    }
+
+    /**
+     * Computes top margin for the textview
+     */
+    public int computeTopMargin(View view, TextView textView, boolean isForTextView) {
+        if (!isForTextView) return computeTopMargin(view);
+
+        boolean isBottom = isOnBottomSide(view);
+
+        if (isBottom) {
+            return (int) (view.getY() - ADJUST_RATIO * view.getHeight());
+        }
+
+        return (int) (view.getY() + 2 * view.getHeight());
     }
 
     /**
--- app/src/main/java/com/zajiclib/infotest/MainActivity.java
+++ app/src/main/java/com/zajiclib/infotest/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button button, button1;
+    Button button, button2;
     ConstraintLayout rootLayout;
     RelativeLayout helpLayout;
     TextView tv;
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        button1 = findViewById(R.id.button2);
+        button2 = findViewById(R.id.button2);
         tv = findViewById(R.id.textView);
         rootLayout = findViewById(R.id.root_layout);
 
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
         generated = new HelpScreen(MainActivity.this, helpLayout);
         generated.addViewsToInfoScreen(button, "Budliky, budliky");
-//        generated.addViewsToInfoScreen(button1, "cukrfliky");
-        //generated.addViewsToInfoScreen(tv, "neco");
+        generated.addViewsToInfoScreen(button2, "Click here to display layout annotations");
+        generated.addViewsToInfoScreen(tv, "hello word annotation");
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                generated.displayAllHelp();
