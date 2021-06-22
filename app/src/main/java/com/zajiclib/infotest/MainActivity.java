a7182d3fa16cfdf7fd299ae8ae9d545d4bae2053
lzajicek <libor.zajicek@kaprion.de>
2021-06-22 17:23:20 +0200

    update

--- app/src/main/java/com/zajiclib/infotest/MainActivity.java
+++ app/src/main/java/com/zajiclib/infotest/MainActivity.java
@@ -3,6 +3,7 @@ package com.zajiclib.infotest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
@@ -20,11 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btn = findViewById(R.id.button);
 
         btn.setOnClickListener((v -> Toast.makeText(this, "budliky", Toast.LENGTH_SHORT).show()));
-
-
     }
 }
